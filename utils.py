2e84ba66d901eb88048a5701427f75ba7bca768b
mdenis <mdrm016@gmail.com>
2022-12-08 12:48:19 -0300

    Correcciones.

--- utils.py
+++ utils.py
@@ -188,7 +188,12 @@ def parse_request(request):
     # Get the interesting values from the request
     values['info_hash'] = to_hex(query_dict['info_hash'])
     values['peer_id'] = to_hex(query_dict['peer_id'])
-    values['ip'] = request.environ['REMOTE_ADDR']  # ignore any sent ip address
+    if request.headers.getlist("X-Forwarded-For"):
+        print("X-Forwarded-For")
+        values['ip'] = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        values['ip'] = request.remote_addr
+    # values['ip'] = request.environ['REMOTE_ADDR']  # ignore any sent ip address
     values['key'] = request.args.get('key', None)
     if request.args.get('event', None):  # Else Update
         values['event'] = request.args.get('event')
